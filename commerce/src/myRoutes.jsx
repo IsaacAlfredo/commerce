e8cb9c6ff1bc9982206ea3de3257a2ebe8c080f3
Isaac Alfredo <isaacfreitas23@gmail.com>
2024-03-17 00:00:27 -0300

    About and contact pages

--- commerce/src/myRoutes.jsx
+++ commerce/src/myRoutes.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import App from "./pages/App/App";
-
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 export function MyRoutes() {
   return (
     <Routes>
       <Route element={<App />} path="/" exact></Route>
-      <Route element={<App />} path="/about"></Route>
-      <Route element={<App />} path="/contact"></Route>
+      <Route element={<About />} path="/about"></Route>
+      <Route element={<Contact />} path="/contact"></Route>
     </Routes>
   );
 }
