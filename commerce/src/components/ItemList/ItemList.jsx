a0fce92ff3b7e485aec23aff969848db720c0bef
Isaac Alfredo <isaacfreitas23@gmail.com>
2024-03-18 00:13:09 -0300

    app page api data

--- commerce/src/components/ItemList/ItemList.jsx
+++ commerce/src/components/ItemList/ItemList.jsx
@@ -1,39 +1,17 @@
 import ItemCard from "../ItemCard/ItemCard";
 import "./ItemList.css";
 
-function ItemList() {
+function ItemList({ data }) {
   return (
     <div className="ItemList">
-      <ItemCard
-        img_src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/latitude-14-3440-laptop/media-gallery/notebook-latitude-14-3440-nt-uma-gray-gallery-1.psd?fmt=pjpg&pscan=auto&scl=1&wid=3300&hei=1999&qlt=100,1&resMode=sharp2&size=3300,1999&chrss=full&imwidth=5000"
-        name="nome"
-        price="3000"
-      />
-      <ItemCard
-        img_src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/latitude-14-3440-laptop/media-gallery/notebook-latitude-14-3440-nt-uma-gray-gallery-1.psd?fmt=pjpg&pscan=auto&scl=1&wid=3300&hei=1999&qlt=100,1&resMode=sharp2&size=3300,1999&chrss=full&imwidth=5000"
-        name="nome"
-        price="3000"
-      />
-      <ItemCard
-        img_src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/latitude-14-3440-laptop/media-gallery/notebook-latitude-14-3440-nt-uma-gray-gallery-1.psd?fmt=pjpg&pscan=auto&scl=1&wid=3300&hei=1999&qlt=100,1&resMode=sharp2&size=3300,1999&chrss=full&imwidth=5000"
-        name="nome"
-        price="3000"
-      />
-      <ItemCard
-        img_src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/latitude-14-3440-laptop/media-gallery/notebook-latitude-14-3440-nt-uma-gray-gallery-1.psd?fmt=pjpg&pscan=auto&scl=1&wid=3300&hei=1999&qlt=100,1&resMode=sharp2&size=3300,1999&chrss=full&imwidth=5000"
-        name="nome"
-        price="3000"
-      />
-      <ItemCard
-        img_src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/latitude-14-3440-laptop/media-gallery/notebook-latitude-14-3440-nt-uma-gray-gallery-1.psd?fmt=pjpg&pscan=auto&scl=1&wid=3300&hei=1999&qlt=100,1&resMode=sharp2&size=3300,1999&chrss=full&imwidth=5000"
-        name="nome"
-        price="3000"
-      />
-      <ItemCard
-        img_src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/latitude-14-3440-laptop/media-gallery/notebook-latitude-14-3440-nt-uma-gray-gallery-1.psd?fmt=pjpg&pscan=auto&scl=1&wid=3300&hei=1999&qlt=100,1&resMode=sharp2&size=3300,1999&chrss=full&imwidth=5000"
-        name="nome"
-        price="3000"
-      />
+      {data.map((card) => (
+        <ItemCard
+          img_src="https://i.dell.com/is/image/DellContent/content/dam/ss2/product-images/dell-client-products/notebooks/latitude-notebooks/latitude-14-3440-laptop/media-gallery/notebook-latitude-14-3440-nt-uma-gray-gallery-1.psd?fmt=pjpg&pscan=auto&scl=1&wid=3300&hei=1999&qlt=100,1&resMode=sharp2&size=3300,1999&chrss=full&imwidth=5000"
+          name={card.name}
+          price={card.price}
+          key={card.id}
+        />
+      ))}
     </div>
   );
 }
--- commerce/src/pages/App/App.jsx
+++ commerce/src/pages/App/App.jsx
@@ -1,13 +1,30 @@
-import { useState } from "react";
 import "./App.css";
 import NavBar from "../../components/NavBar/NavBar";
 import CategoryContainer from "../../components/CategoryContainer/CategoryContainer";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [itemData, setItemData] = useState({ data: [] });
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const itemDataHandler = await axios.get(
+          "http://localhost:8000/api/products"
+        );
+        setItemData({ data: itemDataHandler.data });
+      } catch (e) {
+        console.log(e);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
       <NavBar />
-      <CategoryContainer />
+      <CategoryContainer data={itemData.data} />
     </>
   );
 }
